3b9185cb6e06494acca2f3e71022aa382bc46933
lakshaybxt <bishtlakshay14@gmail.com>
2025-05-06 23:50:52 +0530

    Add Entities for Blog

---
+++ blog-be/src/main/java/com/lakshay/blog_be/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.lakshay.blog_be.domain;
+
+public enum PostStatus {
+    DRAFT, PUBLISHED
+}
---
+++ blog-be/src/main/java/com/lakshay/blog_be/domain/entities/Post.java
@@ -0,0 +1,71 @@
+package com.lakshay.blog_be.domain.entities;
+
+import com.lakshay.blog_be.domain.PostStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "post")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "Text")
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+
+    @Column(nullable = false)
+    private Integer readingTime;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(content, post.content) && status == post.status && Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, status, readingTime, createdAt, updatedAt);
+    }
+
+    // It is automatically invoked by the JPA provider (like Hibernate) right before it inserts the entity into the database.
+    // Only call when entity is created
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    // This will automatically call when entity is updated
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
